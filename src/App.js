85c0814937786d1965a2fa0eb5a6688d31b5b6c1
Rian Fiore <74125852+Rianfiore@users.noreply.github.com>
2022-09-03 19:09:50 -0300

    fix(faq): add component and remove styled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import {Faq} from "components";
+
 function App() {
   return (
     <div className="App">
+      <Faq/>
     </div>
   );
 }
--- src/components/faq/index.js
+++ src/components/faq/index.js
@@ -1,14 +1,13 @@
 import { Icon } from "components/icon"
-import * as S from "./styles.js"
 
 export const Faq = () => {
 
   return (
-    <S.Container className="faq">
+    <div className="faq">
       <div className="faq_content">
         <h1 className="faq_content_title">Como podemos saber como resolver os problemas?</h1>
         <Icon name="arrow"/>
       </div>
-    </S.Container>
+    </div>
   )
 }
