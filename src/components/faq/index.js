f046066c1018759a438f856d16393243dd751acc
Rian Fiore <74125852+Rianfiore@users.noreply.github.com>
2022-09-03 17:52:06 -0300

    add initial structure faq

---
+++ src/components/faq/index.js
@@ -0,0 +1,13 @@
+import { Icon } from "../icon"
+
+export const Faq = () => {
+
+  return (
+    <div>
+      <div>
+        <h1>Como podemos saber como resolver os problemas?</h1>
+        <Icon name="arrow"/>
+      </div>
+    </div>
+  )
+}
