a6d9ecbddbae4982c90558e45ddb942ff6c909e4
ryancardoso <ryanferreiracardoso864@gmail.com>
2022-09-04 01:58:59 -0300

    creating route system

--- src/pages/form/index.js
+++ src/pages/form/index.js
@@ -4,16 +4,10 @@ import { Faq } from "components";
 // Data
 import { dataFAQ } from "data/faq.data";
 
-// Pages
-import { DashboardPage } from "./pages";
-
-function App() {
+export const Form = () => {
   return (
     <div className="App">
       <Faq options={dataFAQ} />
-      <DashboardPage />
     </div>
   );
-}
-
-export default App;
+};
