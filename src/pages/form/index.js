74f2dbb3f8c7678063637e43e69c9f12b823569b
Felipe Silva <felipe.rl.silva@hotmail.com>
2022-09-04 02:24:25 -0300

    adapting to the route system

--- src/pages/form/index.js
+++ src/pages/form/index.js
@@ -4,7 +4,7 @@ import { Faq , Form } from "components";
 // Data
 import { dataFAQ } from "data/faq.data";
 
-export const Form = () => {
+export const FormPage = () => {
   return (
     <div className="App">
       <Form />
