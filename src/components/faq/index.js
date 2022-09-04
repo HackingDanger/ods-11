c93657c135c18bec781d825dfef0baf9e18bc161
ryancardoso <ryanferreiracardoso864@gmail.com>
2022-09-04 03:35:07 -0300

    creating faq section title

--- src/components/faq/index.js
+++ src/components/faq/index.js
@@ -5,9 +5,22 @@ import * as S from "./styles";
 // Components
 import { Accordion } from "components";
 
+// Images
+import BgFAQ from "images/bg-faq.svg";
+import BgGreenFAQ from "images/bg-green-faq.svg";
+import BgGrayFAQ from "images/bg-gray-faq.svg";
+
 export const Faq = ({ options }) => {
   return (
     <S.Container>
+      <S.Header>
+        <S.BoxBg>
+          <img src={BgFAQ} alt="background" />
+          <img src={BgGreenFAQ} alt="background" />
+          <img src={BgGrayFAQ} alt="background" />
+        </S.BoxBg>
+        <h2>PERGUNTAS FREQUENTES</h2>
+      </S.Header>
       {options?.map((item) => (
         <Accordion
           key={item.title + Math.random() * 100}
--- src/components/faq/styles.js
+++ src/components/faq/styles.js
@@ -10,8 +10,57 @@ export const Container = styled.section`
     > div {
       background: #408865;
       :nth-child(2) {
-        background: #006032; 
+        background: #006032;
       }
+    }
+  }
+`;
+
+export const Header = styled.div`
+  position: relative;
+  width: 100%;
+  height: 465px;
+
+  display: flex;
+  align-items: flex-end;
+
+  margin-bottom: 65px;
+
+  h2 {
+    width: 227px;
+    padding: 29px;
+
+    color: #b98b13;
+    font-size: 40px;
+
+    z-index: 5;
+  }
+`;
+
+export const BoxBg = styled.figure`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+
+  img {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+
+    :first-child {
+      z-index: 3;
+    }
+
+    :nth-child(2) {
+      bottom: 8%;
+      z-index: 2; 
+    }
+
+    :last-child {
+      bottom: -14%;
+      z-index: 1;
     }
   }
 `;
